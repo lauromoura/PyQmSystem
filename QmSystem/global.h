1fd08d9e2d16edb5648b4d650bc75224fdad532d
Lauro Neto <lauro.neto@openbossa.org>
2011-07-25 20:57:04 -0300

    Initial version of QmSystem bindings.
    Based on PySide-Mobility cmake files, this version
    compiles and imports in the Harmattan SDK (both targets).
    Not tested on the device yet.
    
    TODO: Add tests and examples, packaging.

---
+++ QmSystem/global.h
@@ -0,0 +1,16 @@
+// Sensors
+// QtMobility global header
+#include "pyside_global.h"
+
+#include <qmactivity.h>
+#include <qmbattery.h>
+#include <qmcabc.h>
+#include <qmdevicemode.h>
+#include <qmdisplaystate.h>
+#include <qmheartbeat.h>
+#include <qmkeys.h>
+#include <qmlocks.h>
+#include <qmsystemstate.h>
+#include <qmthermal.h>
+#include <qmtime.h>
+
