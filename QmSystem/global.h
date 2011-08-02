84ff932b386ac6543afc2cd05eff532596263e77
Lauro Neto <lauro.neto@openbossa.org>
2011-08-02 16:19:53 -0300

    Add support to missing classes

--- QmSystem/global.h
+++ QmSystem/global.h
@@ -2,15 +2,24 @@
 // QtMobility global header
 #include "pyside_global.h"
 
+#include <qmaccelerometer.h>
 #include <qmactivity.h>
+#include <qmals.h>
 #include <qmbattery.h>
 #include <qmcabc.h>
+#include <qmcompass.h>
 #include <qmdevicemode.h>
 #include <qmdisplaystate.h>
 #include <qmheartbeat.h>
 #include <qmkeys.h>
+#include <qmled.h>
 #include <qmlocks.h>
+#include <qmmagnetometer.h>
+#include <qmorientation.h>
+#include <qmproximity.h>
+#include <qmrotation.h>
 #include <qmsystemstate.h>
+#include <qmtap.h>
 #include <qmthermal.h>
 #include <qmtime.h>
 
